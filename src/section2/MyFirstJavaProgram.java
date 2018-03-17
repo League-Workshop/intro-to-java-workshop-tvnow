351027e24180ef41c2b95b35288cbfe157ffbe9b
RationalGeographic <league@WTS7.attlocal.net>
2018-03-17 12:29:30 -0700

    yu gi oh

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot robber=new Robot();
+	robber.setSpeed(4000);
+	robber.move (250);
+	robber.turn(-90);
+		robber.move(350);
+		robber.turn(-90);
+		robber.move(450);
+		robber.turn(-90);
+	}
 	
-		
+	void draw() {
 		
 	}
 }
